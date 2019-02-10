1d104e4076f09b2c50f8c69fda4eb5bf09cf8762
Andee13 <39676479+Andee13@users.noreply.github.com>
2019-02-11 00:35:34 +0200

    Code improve style of code

--- q.js
+++ q.js
@@ -1,162 +1,83 @@
-let table = document.getElementById("table");
-let addButtonColumn = document.getElementById("addButtonColumn");
-let addButtonRow = document.getElementById("addButtonRow");
-//ad2.onclick = addRow;
-//adding column
-addButtonColumn.onclick = addColumn;
-function addColumn() {
-    let rows = document.getElementsByTagName("tr");
-    let arr = document.getElementsByTagName("tr");
-    for(let i = 0; i < arr.length; i++){
-        let elem = document.createElement("td");
-        arr[i].appendChild(elem);
-    }
-    // if( table.rows[0].cells.length >= 2){
-    //     removeButtonColumn.style.visibility = "visible";
-    // }
-};
-//adding row
-addButtonRow.onclick = addRow;
+let table = document.querySelector("#table");
+let addButtonColumn = document.querySelector("#addButtonColumn");
+let addButtonRow = document.querySelector("#addButtonRow");
+let removeButtonColumn = document.querySelector("#minusButtonColumn");
+let removeRow = document.querySelector("#minusButtonRow");
+
+addButtonColumn.addEventListener('click',addColumn);
+addButtonRow.addEventListener('click', addRow);
+removeButtonColumn.addEventListener('click', removeColumn);
+removeRow.addEventListener('click', removeRows);
+table.addEventListener('mouseover', move);
+table.addEventListener('mouseleave', hide);
+removeRow.addEventListener('mouseover', makeVisible);
+removeButtonColumn.addEventListener('mouseover', makeVisible);
+
 function addRow() {
-    var addButton2 = document.getElementById("addButton2");
-    console.log(table.childNodes[1]);
     let row = document.createElement("tr");
-    let n = document.getElementsByTagName("tr")[0].children.length;
-    for(let i = 0; i < n; i++){
+    let length = document.querySelector("tr").children.length;
+    for (let i = 0; i < length; i++) {
         let td = document.createElement("td");
         row.appendChild(td);
     }
-    console.log(table.getElementsByTagName("tbody")[0]);
-    table.getElementsByTagName("tbody")[0].appendChild(row);
-    //set Visible
-    // if(table.rows.length >=2 ){
-    //     removeRow.style.visibility = "visible";
-    // }
+    table.querySelector("tbody").appendChild(row);
+    console.log(table.querySelector("tbody").appendChild(row));
 }
-
-
-let removeButtonColumn = document.getElementById("minusButtonColumn");
-removeButtonColumn.onclick = removeColumn;
+function addColumn() {
+    let array = document.querySelectorAll("tr");
+    let length = array.length;
+    for (let i = 0; i < length; i++) {
+        let elem = document.createElement("td");
+        array[i].append(elem);
+    }
+}
 function removeColumn(){
-
-    let rows = document.getElementsByTagName("tr");
-    console.log(table.getElementsByTagName("tbody"));
-    for (let i = 0; i < rows.length; i++) {
+    let rows = document.querySelectorAll("tr");
+    let length = rows.length;
+    for (let i = 0; i < length; i++) {
         table.rows[i].cells[0].remove();
     }
-
-    let cells = table.rows[0].cells.length;
-    //console.log(cells);
-    //if(cells < 2 ) {
-    removeButtonColumn.style.visibility = "hidden";
-    removeRow.style.visibility = "hidden";
-    //}
-    //removeButtonColumn.style.visibility = "hidden";
+    hide();
 }
-
-let removeRow = document.getElementById("minusButtonRow");
-removeRow.onclick = removeRows;
 function removeRows() {
-    let rows = document.getElementsByTagName("tr");
     table.rows[0].remove();
-
-
-    removeButtonColumn.style.visibility = "hidden";
-    removeRow.style.visibility = "hidden";
-
+    hide();
 }
-
-table.onmouseover = move;
-table.onmouseleave = leave;
-removeRow.onmouseover = makeVisible;
-removeButtonColumn.onmouseover = makeVisible;
 function move(event) {
-    //target.style.marginLeft = (parseInt(window.getComputedStyle(target).marginLeft) + 54).toString()+"px";
     makeVisible();
     let elem = event.target;
-    let m = window.getComputedStyle(elem);
     if(elem.tagName === "TD"){
         let indexLeft = 0;
         //find a column
         for(let i = 0;i < elem.parentNode.children.length; i++){
             if(elem ===  elem.parentNode.children.item(i)) {
-               // console.log("elem = ",  i);
                 indexLeft = i;
                 break;
             }
-
         }
-
-        //
         let left = 103 + 53.8 * indexLeft;
-
-        //change style via style
-        //console.log("index = ", window.getComputedStyle(removeButtonColumn).left);
-        // removeButtonColumn.style.left = "103px";
-        // console.log("1111111 = " + left);
-        // console.log("1111111 = " + indexLeft);
-        // console.log("1111111 = " + window.getComputedStyle(removeButtonColumn).left);
-        //
-        // removeButtonColumn.style.left = ((parseInt(window.getComputedStyle(removeButtonColumn).left) + left).toString() + "px");
-
         removeButtonColumn.style.left = left + "px";
-        //
-        //find a row
         let indexTop = 0;
         for(let i = 0;i < elem.parentNode.parentNode.children.length; i++){
             if(elem.parentNode ===  elem.parentNode.parentNode.children.item(i)) {
-                console.log("elem row = ",  i);
                 indexTop = i;
                 break;
             }
         }
-
-        //code comment try doing
-        console.log(indexTop);
         let top = 103 + indexTop * 52.8;
-        // removeRow.style.top = "103px";
-        // removeRow.style.top = ((parseInt(window.getComputedStyle(removeRow).top) + top).toString() + "px");
-        //
-        // let interval = parseInt(window.getComputedStyle(removeRow).top)  + top;
-        // console.log("interval = ", interval);
         removeRow.style.top = top + "px";
-        //
-
-
-         // let timer = setInterval(function() {
-         //     if (interval <= window.getComputedStyle(removeRow).top) {
-         //         clearInterval(timer); // конец через 1 секунду
-         //         return;
-         //     }
-         //     let nf = parseInt(window.getComputedStyle(removeRow).top) + 1;
-         //     removeRow.style.top = nf + "px";
-         //     }, 100);
-
-        //
         window.getComputedStyle(removeButtonColumn);
-        //console.log();
-
-
     }
-    //console.log(event.target.tagName)
 }
 function  makeVisible() {
     if(table.rows.length >=2 ) {
         removeRow.style.visibility = "visible";
     }
-
-    // removeRow.onclick = function () {
-    //     removeRow.style.visibility = "hidden";
-    // }
     if( table.rows[0].cells.length >= 2){
             removeButtonColumn.style.visibility = "visible";
     }
-
-    // removeRow.style.visibility = "visible";
-    // removeButtonColumn.style.visibility = "visible";
 }
-
-function leave (){
-    removeRow.style.visibility = "hidden";
+function hide() {
     removeButtonColumn.style.visibility = "hidden";
+    removeRow.style.visibility = "hidden";
 }
