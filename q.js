6a781b73dda3ddebaab0660a1f9de58bdc5257d4
Andee13 <39676479+Andee13@users.noreply.github.com>
2019-02-12 01:30:31 +0200

    adding % position, add Array.from() and Array.indexOf(), adding arrow functions

--- q.js
+++ q.js
@@ -1,74 +1,59 @@
+let scene = document.querySelector("#scene");
 let table = document.querySelector("#table");
 let addButtonColumn = document.querySelector("#addButtonColumn");
 let addButtonRow = document.querySelector("#addButtonRow");
 let removeButtonColumn = document.querySelector("#minusButtonColumn");
 let removeRow = document.querySelector("#minusButtonRow");
 
-addButtonColumn.addEventListener('click',addColumn);
-addButtonRow.addEventListener('click', addRow);
-removeButtonColumn.addEventListener('click', removeColumn);
-removeRow.addEventListener('click', removeRows);
-table.addEventListener('mouseover', move);
-table.addEventListener('mouseleave', hide);
-removeRow.addEventListener('mouseover', makeVisible);
-removeButtonColumn.addEventListener('mouseover', makeVisible);
+let lastRowPosition;
+let lastColumnPosition;
 
-function addRow() {
-    let row = document.createElement("tr");
-    let length = document.querySelector("tr").children.length;
-    for (let i = 0; i < length; i++) {
-        let td = document.createElement("td");
-        row.appendChild(td);
-    }
-    table.querySelector("tbody").appendChild(row);
-    console.log(table.querySelector("tbody").appendChild(row));
-}
-function addColumn() {
-    let array = document.querySelectorAll("tr");
-    let length = array.length;
-    for (let i = 0; i < length; i++) {
-        let elem = document.createElement("td");
-        array[i].append(elem);
-    }
-}
-function removeColumn(){
+addButtonColumn.addEventListener('click',() => {
+        let array = document.querySelectorAll("tr");
+        let length = array.length;
+        for (let i = 0; i < length; i++) {
+            let elem = document.createElement("td");
+            array[i].append(elem);
+        }});
+addButtonRow.addEventListener('click', () => {
+        let row = document.createElement("tr");
+        let length = document.querySelector("tr").children.length;
+        for (let i = 0; i < length; i++) {
+            let td = document.createElement("td");
+            row.appendChild(td);
+        }
+        table.querySelector("tbody").appendChild(row);
+    });
+removeButtonColumn.addEventListener('click', () => {
     let rows = document.querySelectorAll("tr");
     let length = rows.length;
     for (let i = 0; i < length; i++) {
-        table.rows[i].cells[0].remove();
+        table.rows[i].cells[lastColumnPosition].remove();
     }
     hide();
-}
-function removeRows() {
-    table.rows[0].remove();
+});
+removeRow.addEventListener('click', () => {
+    table.rows[lastRowPosition].remove();
     hide();
-}
-function move(event) {
-    makeVisible();
+});
+table.addEventListener('mouseover', () =>{
     let elem = event.target;
-    if(elem.tagName === "TD"){
-        let indexLeft = 0;
-        //find a column
-        for(let i = 0;i < elem.parentNode.children.length; i++){
-            if(elem ===  elem.parentNode.children.item(i)) {
-                indexLeft = i;
-                break;
-            }
-        }
-        let left = 103 + 53.8 * indexLeft;
-        removeButtonColumn.style.left = left + "px";
-        let indexTop = 0;
-        for(let i = 0;i < elem.parentNode.parentNode.children.length; i++){
-            if(elem.parentNode ===  elem.parentNode.parentNode.children.item(i)) {
-                indexTop = i;
-                break;
-            }
-        }
-        let top = 103 + indexTop * 52.8;
-        removeRow.style.top = top + "px";
-        window.getComputedStyle(removeButtonColumn);
+    if(elem.tagName === "TD") {
+        let numberOfTr = elem.parentElement.parentElement.children.length;
+        let arrayTr = Array.from(table.children[0].children);
+        lastRowPosition = arrayTr.indexOf(elem.parentElement);
+        removeRow.style.top = ((100 / numberOfTr) * lastRowPosition) + "%";
+
+        let numberOfTd = elem.parentElement.children.length;
+        let arrayTd = Array.from(table.children[0].children[lastRowPosition].children);
+        lastColumnPosition = arrayTd.indexOf(elem);
+        removeButtonColumn.style.left = ((100 / numberOfTd) * lastColumnPosition) + "%";
     }
-}
+});
+table.addEventListener('mouseleave', hide);
+scene.addEventListener('mouseover', makeVisible);
+
+
 function  makeVisible() {
     if(table.rows.length >=2 ) {
         removeRow.style.visibility = "visible";
